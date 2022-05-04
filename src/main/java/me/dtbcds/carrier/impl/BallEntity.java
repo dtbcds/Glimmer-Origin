2516da7357842b7f28406ddc9a8959a1f2c77bce
dtb <93344626+dtbcds@users.noreply.github.com>
2022-05-04 11:11:26 +0300

    Update BallEntity.java

--- src/main/java/me/dtbcds/carrier/impl/BallEntity.java
+++ src/main/java/me/dtbcds/carrier/impl/BallEntity.java
@@ -4,9 +4,18 @@ import net.minecraft.entity.EntityType;
 import net.minecraft.entity.decoration.ArmorStandEntity;
 import net.minecraft.text.Text;
 import net.minecraft.world.World;
+import software.bernie.geckolib3.core.IAnimatable;
+import software.bernie.geckolib3.core.PlayState;
+import software.bernie.geckolib3.core.builder.AnimationBuilder;
+import software.bernie.geckolib3.core.controller.AnimationController;
+import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
+import software.bernie.geckolib3.core.manager.AnimationData;
+import software.bernie.geckolib3.core.manager.AnimationFactory;
 
-public class BallEntity extends ArmorStandEntity{
+public class BallEntity extends ArmorStandEntity implements IAnimatable{
 	private int lifeTime;
+	private AnimationFactory factory = new AnimationFactory(this);
+	public AnimationBuilder idle = new AnimationBuilder().addAnimation("load.anim" , true);
 
 	public BallEntity(EntityType<? extends ArmorStandEntity> type, World world) {
 		super(type, world);
@@ -24,5 +33,18 @@ public class BallEntity extends ArmorStandEntity{
 		if(lifeTime>45) {
 			this.remove(RemovalReason.KILLED);
 		}
+	}
+	private <E extends IAnimatable> PlayState animate(AnimationEvent<E> event) {
+            event.getController().setAnimation(idle);
+            return PlayState.CONTINUE;
+	}
+	@Override
+	public void registerControllers(AnimationData data) {
+		data.addAnimationController(new AnimationController<BallEntity>(this, "controller", 0, this::animate));
+		
+	}
+	@Override
+	public AnimationFactory getFactory() {
+		return this.factory;
 	}
 }
