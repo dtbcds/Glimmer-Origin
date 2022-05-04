3d197467502bffb8227379a736063154f71f3cf9
dtb <93344626+dtbcds@users.noreply.github.com>
2022-05-04 11:10:35 +0300

    Update BallEntityRenderer.java

--- src/main/java/me/dtbcds/carrier/api/BallEntityRenderer.java
+++ src/main/java/me/dtbcds/carrier/api/BallEntityRenderer.java
@@ -1,19 +1,21 @@
 package me.dtbcds.carrier.api;
 
-import me.dtbcds.carrier.CarrierClient;
 import me.dtbcds.carrier.impl.BallEntity;
 import net.minecraft.client.render.entity.EntityRendererFactory;
-import net.minecraft.client.render.entity.LivingEntityRenderer;
-import net.minecraft.util.Identifier;
+import software.bernie.geckolib3.renderers.geo.GeoEntityRenderer;
 
-public class BallEntityRenderer extends LivingEntityRenderer<BallEntity, BallEntityModel> {
+public class BallEntityRenderer extends GeoEntityRenderer<BallEntity> {
+
+	public BallEntityRenderer(EntityRendererFactory.Context ctx) {
+		super(ctx, new BallEntityModel());
+	}
 	 
-    public BallEntityRenderer(EntityRendererFactory.Context context) {
-        super(context, new BallEntityModel(context.getPart(CarrierClient.MODEL_BALL_LAYER)), 0.5f);
-    }
- 
-    @Override
-    public Identifier getTexture(BallEntity entity) {
-        return new Identifier("carrier", "textures/entity/ball.png");
-    }
+//    public BallEntityRenderer(EntityRendererFactory.Context context) {
+//        super(context, new BallEntityModel(context.getPart(CarrierClient.MODEL_BALL_LAYER)), 0.5f);
+//    }
+// 
+//    @Override
+//    public Identifier getTexture(BallEntity entity) {
+//        return new Identifier("carrier", "textures/entity/ball.png");
+//    }
 }
